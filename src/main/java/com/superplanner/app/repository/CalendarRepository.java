d5f2cb3f721e903102318b04d7ba8654d19f2983
LucasFrigo <lukeg299@gmail.com>
2023-12-01 10:56:52 -0300

    fix - refatorado a estrutura JPA pra ficar de acordo com o as specs do mongodb

--- src/main/java/com/superplanner/app/repository/CalendarRepository.java
+++ src/main/java/com/superplanner/app/repository/CalendarRepository.java
@@ -1,5 +1,6 @@
 package com.superplanner.app.repository;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.superplanner.app.model.Day;
@@ -9,28 +10,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
-public class CalendarRepository {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public Month findMonthByYearAndMonth(int year, Long month) {
-        return entityManager.createQuery("SELECT m FROM Month m WHERE m.year = :year AND m.month = :month", Month.class)
-                 .setParameter("year", year)
-                 .setParameter("month", month)
-                 .getSingleResult();
-    }
-
-    public Day findDayById(Long dayId) {
-        return entityManager.find(Day.class, dayId);
-    }
-
-    public void saveDay(Day day) {
-        if (day.getDayId() == null) {
-             entityManager.persist(day);
-         } else {
-             entityManager.merge(day);
-         }
-    }
+public interface CalendarRepository extends MongoRepository<Month, Long> {
+    
+    Month findByYearAndMonth(int year, Long month);
+    Day findByDayId(String dayId);
     
 }
